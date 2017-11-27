c4211c3d501f2edab9c4d344025a900ea43ebd31
LifeIsGoodMario <32020789+LifeIsGoodMario@users.noreply.github.com>
2017-11-27 14:31:04 +0100

    Add files via upload

---
+++ AStarMaster.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class AStarMaster : MonoBehaviour
+{
+    public int width, length;
+
+    public Vector2 start, goal;
+
+    private Grid2D grid;
+    public Algorithm algorithm;
+
+    public Cell[] path;
+
+
+    private void Awake()
+    {
+        grid = new Grid2D(width, length);
+        algorithm = new Algorithm(grid, start, goal);
+    }
+
+
+    private void Start()
+    {
+        grid.Generate();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            path = algorithm.AStarSearch();
+    }
+}
---
+++ Cell.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Cell
+{
+    public Vector2 position;
+
+    public float cost, heuristic;
+    public float f
+    {
+        get { return cost + heuristic; }
+    }
+
+    public Cell parent;
+
+
+    private Cell() { }
+
+    public Cell(Vector2 position)
+    {
+        this.position = position;
+    }
+}
