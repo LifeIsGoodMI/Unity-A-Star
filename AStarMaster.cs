6a79ab8f55012ca0b34660117d94b02b80c4332d
Mario Infantino <32020789+GoGreenOrDieTryin@users.noreply.github.com>
2017-12-07 00:30:45 +0100

    Update AStarMaster.cs

--- AStarMaster.cs
+++ AStarMaster.cs
@@ -36,9 +36,11 @@ public class AStarMaster : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return))
             path = algorithm.AStarSearch(); // Receive the constructed path.
 
-        // Let a actor follow the path, really really basic.
+        // Lets an actor follow the path, really really basic.
         if (path != null && path.Length > 0)
         {
+            // Ideally, you would *not* be doing this stuff every frame. 
+            // Just keeping it simple here :)
             var positions = new List<Vector3>();
             path.ToList().ForEach(c => positions.Add(new Vector3(c.position.x, 0, c.position.y)));
 
