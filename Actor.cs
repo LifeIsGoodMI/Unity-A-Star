14c888f6d7d05f6e9423994bbee0fae660121453
Mario Infantino <32020789+GoGreenOrDieTryin@users.noreply.github.com>
2017-12-07 00:12:36 +0100

    Add files via upload

---
+++ Actor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    private int curIndex;
+
+
+    public void Move(Vector3[] path)
+    {
+        if ((path[curIndex] - transform.position).magnitude < 0.4f)
+            curIndex++;
+        if (curIndex >= path.Length)
+            curIndex = 0;
+
+        var direction = path[curIndex] - transform.position;
+
+        if (direction != Vector3.zero)
+        {
+            var targetRot = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * 2.0f);
+        }
+
+        var velocity = transform.forward * movementSpeed * Time.deltaTime;
+        transform.position += velocity;
+    }
+}
